7763f41fa3d0ca8d9989798821b4be1b4b511c50
Vincent <liwensen@ethank.com.cn>
2016-09-10 23:50:26 +0800

    init

---
+++ wintip.js
@@ -0,0 +1,17 @@
+function winTip(msg, id) {
+  var tid = id || 1,
+      query = function(selector) {return document.querySelectorAll(selector)},
+      append = function(ele, html) {query(ele)[0].insertAdjacentHTML('beforeend', html)};
+  var tip = '<span id="tip_' + tid + '" class="_win_tip" style="display: inline-block;min-width: 80px;padding: 10px;margin-bottom: 1px;background-color: rgba(0, 0, 0, .75);">' + msg + '</span><br>',
+      tipBox = '<div class="_win_tip_box" style="position: fixed;top: 0;left: 0;color: #fff;z-index: 100;cursor: move" draggable="true">' + tip + '</div>';
+  var ele = query('#tip_' + tid);
+  if (ele.length > 1) {
+    ele.textContent = msg;
+  } else if (query('._win_tip_box').length > 0) {
+    append('._win_tip_box', tip);
+  } else {
+    append('body', tipBox);
+  }
+  console.log(msg);
+  return ele;
+}
