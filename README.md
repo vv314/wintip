01695f9663c394560883d56b04d7f84040f0f557
Vincent <liwensen@ethank.com.cn>
2016-09-12 12:26:49 +0800

    add feature

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # win-tip
-创建简单的窗口提示框，移动端设备调试用。
+创建简单的窗口提示框，移动端设备注入调试用。
+
+- wintip.js  标准版
+- wintip.lite.js  精简版
+
+## Usage
+页面引入wintip.js，暴露全局函数winTip
+```
+<script src="wintip.js"></script>
+```
+
+    // @TODO add demo
--- wintip.js
+++ wintip.js
@@ -1,17 +1,40 @@
-function winTip(msg, id) {
-  var tid = id || 1,
-      query = function(selector) {return document.querySelectorAll(selector)},
-      append = function(ele, html) {query(ele)[0].insertAdjacentHTML('beforeend', html)};
-  var tip = '<span id="tip_' + tid + '" class="_win_tip" style="display: inline-block;min-width: 80px;padding: 10px;margin-bottom: 1px;background-color: rgba(0, 0, 0, .75);">' + msg + '</span><br>',
-      tipBox = '<div class="_win_tip_box" style="position: fixed;top: 0;left: 0;color: #fff;z-index: 100;cursor: move" draggable="true">' + tip + '</div>';
-  var ele = query('#tip_' + tid);
-  if (ele.length > 1) {
-    ele.textContent = msg;
-  } else if (query('._win_tip_box').length > 0) {
-    append('._win_tip_box', tip);
-  } else {
-    append('body', tipBox);
+function winTip(msg, name) {
+  var id = query('._win_tip').length + 1,
+      tip = query('._tip_' + (name?name:id)),
+      tipBox = query('._win_tip_box'),
+      idStr, tipHtml, tipBoxHtml;
+
+  if(likeNumber(name) && !tip.length) {
+    alert('wintip:' + name + ' is not defined');
+    return;
   }
-  console.log(msg);
-  return ele;
+
+  function query(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+  function append(ele, html) {
+    if(typeof(ele) === 'object' && ele.nodeType === 1) {
+      ele.insertAdjacentHTML('beforeend', html);
+    } else {
+      query(ele)[0].insertAdjacentHTML('beforeend', html);
+    }
+  }
+  
+  function likeNumber(value) {
+    return !isNaN(Number(value));
+  }
+
+  idStr = (likeNumber(name) || typeof(name) === 'undefined')?'_tip_'+id:'_tip_'+id+' _tip_'+name;
+  tipHtml = '<span class="_win_tip ' + idStr + '" style="display: inline-block;min-width: 80px;padding: 10px;margin-bottom: 1px;background-color: rgba(0, 0, 0, .75);">' + msg + '</span><br>';
+  tipBoxHtml = '<div class="_win_tip_box" style="position: fixed;top: 0;left: 0;max-height: 75%;max-width: 55%;color: #fff;font-size: 12px;z-index: 100;overflow: hidden">' + tipHtml + '</div>';
+
+  if (tip.length) {
+    tip[0].textContent = msg;
+  } else if (tipBox.length) {
+    append(tipBox[0], tipHtml);
+  } else {
+    append('body', tipBoxHtml);
+  }
+  return tip;
 }
