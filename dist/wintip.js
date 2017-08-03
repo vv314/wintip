be20ec1912f0e64601bc3cf688b59011f8d42bf3
Vincent Lee <vv314@foxmail.com>
2017-08-04 00:49:11 +0800

    支持$ 选择指定 tip

--- dist/wintip.js
+++ dist/wintip.js
@@ -9,6 +9,10 @@ function query(selector) {
   return document.querySelectorAll(selector)
 }
 
+function likeNumber(value) {
+  return !isNaN(Number(value))
+}
+
 function isElement(ele) {
   return typeof ele === 'object' && ele.nodeType === 1
 }
@@ -61,7 +65,36 @@ winTip.config = function (options) {
   Object.assign(settings, options);
 };
 
-winTip.named = function (name) {};
+winTip.$ = function (name) {
+  var idNo = query('._win_tip').length + 1;
+  var tip = query(("._tip_" + name));
+  var tipBox = query('._win_tip_box');
+
+  if (likeNumber(name) && !tip.length) {
+    return alert(("wintip: " + name + " is not defined"))
+  }
+
+  return function(msg) {
+    var idStr =
+      likeNumber(name) || typeof name === 'undefined'
+        ? ("_tip_" + idNo)
+        : ("_tip_" + idNo + " _tip_" + name);
+
+    var tipHtml = "<span class=\"_win_tip " + idStr + "\" style=\"display: inline-block;min-width: 80px;padding: 8px;margin-bottom: 1px;background-color: rgba(0, 0, 0, " + (settings.opacity) + ");\">" + msg + "</span><br>";
+
+    var tipBoxHtml = "<div class=\"_win_tip_box\" style=\"position: fixed;top: 0;left: 0;max-height: 75%;word-break: break-all;max-width: 55%;color: #fff;font-size: 12px;z-index: 100;overflow: auto;-webkit-overflow-scrolling: touch;\">" + tipHtml + "</div>";
+
+    if (tip.length) {
+      tip[0].textContent = msg;
+    } else if (tipBox.length) {
+      append(tipBox[0], tipHtml);
+    } else {
+      append('body', tipBoxHtml);
+    }
+
+    return tip
+  }
+};
 
 return winTip;
 
--- src/wintip.js
+++ src/wintip.js
@@ -58,6 +58,35 @@ winTip.config = options => {
   Object.assign(settings, options)
 }
 
-winTip.named = name => {}
+winTip.$ = name => {
+  const idNo = query('._win_tip').length + 1
+  const tip = query(`._tip_${name}`)
+  const tipBox = query('._win_tip_box')
+
+  if (likeNumber(name) && !tip.length) {
+    return alert(`wintip: ${name} is not defined`)
+  }
+
+  return function(msg) {
+    const idStr =
+      likeNumber(name) || typeof name === 'undefined'
+        ? `_tip_${idNo}`
+        : `_tip_${idNo} _tip_${name}`
+
+    const tipHtml = `<span class="_win_tip ${idStr}" style="display: inline-block;min-width: 80px;padding: 8px;margin-bottom: 1px;background-color: rgba(0, 0, 0, ${settings.opacity});">${msg}</span><br>`
+
+    const tipBoxHtml = `<div class="_win_tip_box" style="position: fixed;top: 0;left: 0;max-height: 75%;word-break: break-all;max-width: 55%;color: #fff;font-size: 12px;z-index: 100;overflow: auto;-webkit-overflow-scrolling: touch;">${tipHtml}</div>`
+
+    if (tip.length) {
+      tip[0].textContent = msg
+    } else if (tipBox.length) {
+      append(tipBox[0], tipHtml)
+    } else {
+      append('body', tipBoxHtml)
+    }
+
+    return tip
+  }
+}
 
 export default winTip
