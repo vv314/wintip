e08337a05fed94f14a910a4e8f5ab2f89999b879
Vincent Lee <vv314@foxmail.com>
2017-08-10 13:57:36 +0800

    add utils.js

---
+++ src/utils.js
@@ -0,0 +1,36 @@
+function query(selector) {
+  return document.querySelector(selector)
+}
+
+function querys(selector) {
+  return document.querySelectorAll(selector)
+}
+
+function createEl(tag) {
+  return document.createElement(tag)
+}
+
+function likeNumber(value) {
+  return !isNaN(Number(value))
+}
+
+function isElement(target) {
+  // Element or Fragment
+  return (
+    (typeof target === 'object' && target.nodeType === 1) ||
+    target.nodeType === 11
+  )
+}
+
+function isFunc(target) {
+  return typeof target === 'function'
+}
+
+function append(ele, dom) {
+  ele = isElement(ele) ? ele : query(ele)
+  ele.appendChild(dom)
+
+  return ele
+}
+
+export { query, querys, createEl, likeNumber, isElement, isFunc, append }
